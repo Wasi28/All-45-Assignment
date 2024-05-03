58e60841e75c209e661f6cea98d9f86f8869aed0
wasi ul haq 28 <kwasi4954@gmail.com>
2024-05-02 21:32:28 -0700

    first commit

---
+++ Assignment 3/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//storing the famous quote and its author
+let quote = "The only way to do great work is to love what you do.";
+let author = "Wasi Khan";
+//printing the message
+console.log(`"${quote}" - ${author} `);
---
+++ Assignment 3/main.ts
@@ -0,0 +1,9 @@
+//storing the famous quote and its author
+
+let quote:string = "The only way to do great work is to love what you do.";
+
+let author:string = "Wasi Khan";
+
+//printing the message
+
+console.log(`"${quote}" - ${author} `);
