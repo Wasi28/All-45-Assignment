8af927f41cbaa0f20730de5dc62b4484a259f143
wasi ul haq 28 <kwasi4954@gmail.com>
2024-05-02 21:11:59 -0700

    first commit

---
+++ Assignment 2/main.js
@@ -0,0 +1,9 @@
+"use strict";
+//storing a person name in a variable
+let personName = "Wasi khan";
+//printing a person name in lowercase
+console.log("lowercase:", personName.toLowerCase());
+//printing a person name in uppercase
+console.log("Uppercase:", personName.toUpperCase());
+//printing a person name in titlecase
+console.log("Titlecase:", toTitleCase(personName));
---
+++ Assignment 2/main.ts
@@ -0,0 +1,15 @@
+//storing a person name in a variable
+
+let personName:string = "Wasi khan"
+
+//printing a person name in lowercase
+
+console.log("lowercase:",personName.toLowerCase());
+
+//printing a person name in uppercase
+
+console.log("Uppercase:",personName.toUpperCase());
+
+//printing a person name in titlecase
+
+console.log("Titlecase:", toTitleCase(personName));
